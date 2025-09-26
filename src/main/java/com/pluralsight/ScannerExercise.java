622f92fde7b7c41070111e24bab6c923f174c923
Mateo Morales <Moralmat76@gmail.com>
2025-09-26 13:26:58 -0500

    ScannerExercise step 2

--- src/main/java/com/pluralsight/ScannerExercise.java
+++ src/main/java/com/pluralsight/ScannerExercise.java
@@ -9,7 +9,13 @@ public class ScannerExercise {
 
         System.out.print("First Name: ");
         String firstName = scanner.nextLine();
-        System.out.println("Hi " + firstName);
+        System.out.println("Hello " + firstName);
+
+        System.out.print("How old are you?: ");
+        Integer Age = scanner.nextInt();
+        System.out.println("You are years old " + Age);
+
+        System.out.print("");
 
     }
 }
